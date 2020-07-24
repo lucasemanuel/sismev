d4ff03f17640b8f24fa421ba08d4d11ad6ca3b17
Lucas Emanuel <lucaxemanuel@gmail.com>
2020-07-24 14:35:56 -0300

    :wrench: Config language application

--- config/web.php
+++ config/web.php
@@ -11,6 +11,8 @@ $config = [
         '@bower' => '@vendor/bower-asset',
         '@npm'   => '@vendor/npm-asset',
     ],
+    'language' => 'pt-BR',
+    'sourceLanguage' => 'en-US',
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
