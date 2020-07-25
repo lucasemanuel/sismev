6d0f1f1c2223a5b2bb05a9dc05a53d2ec8eb4033
Lucas Emanuel <lucaxemanuel@gmail.com>
2020-07-25 18:11:56 -0300

    :lipstick: Stylizing login page and sign up

--- views/signup/signup_company.php
+++ views/signup/signup_company.php
@@ -1,32 +1,43 @@
 <?php
 
-use yii\bootstrap\ActiveForm;
-use yii\bootstrap\Html;
+use yii\bootstrap4\ActiveForm;
+use yii\bootstrap4\Html;
 
-$this->title = 'Sign Up';
-$this->params['breadcrumbs'][] = $this->title;
+$this->title = Yii::t('app', 'Sign Up - Company');
+
 ?>
-<div class="site-signup">
-    <h1><?= Html::encode($this->title) ?></h1>
-    <div class="row">
-        <div class="col-lg-5">
-
-            <?php $form = ActiveForm::begin(); ?>
-
-                <?= $form->field($model, 'name')->textInput(['autofocus' => true, 'maxlength' => true]) ?>
-
-                <?= $form->field($model, 'trade_name')->textInput(['maxlength' => true]) ?>
-
-                <?= $form->field($model, 'ein')->textInput(['maxlength' => true]) ?>
-
-                <?= $form->field($model, 'email')->textInput(['maxlength' => true]) ?>
-
-                <div class="form-group">
-                    <?= Html::submitButton('Submit', ['class' => 'btn btn-primary']) ?>
+<div class="container">
+    <div class="card o-hidden border-0 shadow-lg my-5">
+        <div class="card-body p-0">
+            <!-- Nested Row within Card Body -->
+            <div class="row">
+            <div class="col-lg-5 d-none d-lg-block bg-register-image"></div>
+                <div class="col-lg-7">
+                    <div class="p-5">
+                        <div class="text-center">
+                            <h1 class="h1 text-gray-900 mb-6 font-weight-bold"><?= Yii::$app->name ?></h1>
+                            <h1 class="h5 text-gray-900 mb-4"><?= $this->title ?></h1>
+                        </div>
+                        <?php $form = ActiveForm::begin(); ?>
+                        <?= $form->field($model, 'name')->textInput(['autofocus' => true, 'maxlength' => true]) ?>
+                        <?= $form->field($model, 'trade_name')->textInput(['maxlength' => true]) ?>
+                        <?= $form->field($model, 'ein')->textInput(['maxlength' => true]) ?>
+                        <?= $form->field($model, 'email')->textInput(['maxlength' => true]) ?>
+                        <div class="form-group">
+                            <div class="row">
+                                <div class="col">
+                                    <?= Html::submitButton(Yii::t('app', 'Next'), ['class' => 'btn btn-primary btn-block']) ?>
+                                </div>
+                            </div>
+                        </div>
+                        <?php ActiveForm::end(); ?>
+                        <hr>
+                        <div class="text-center">
+                            <?= Html::a(Yii::t('app', 'Login'), ['site/login']); ?>
+                        </div>
+                    </div>
                 </div>
-
-            <?php ActiveForm::end(); ?>
-
+            </div>
         </div>
     </div>
 </div>
--- views/site/login.php
+++ views/site/login.php
@@ -7,58 +7,41 @@
 use yii\helpers\Html;
 use yii\bootstrap4\ActiveForm;
 
-$this->title = 'Login';
+$title = Yii::t('app', 'Login');
+
 ?>
 
 <div class="container">
-
-  <!-- Outer Row -->
-  <div class="row justify-content-center">
-
-    <div class="col-xl-10 col-lg-12 col-md-9">
-
-      <div class="card o-hidden border-0 shadow-lg my-5">
-        <div class="card-body p-0">
-          <!-- Nested Row within Card Body -->
-          <div class="row">
-            <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
-            <div class="col-lg-6">
-              <div class="p-5">
-                <div class="text-center">
-                  <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
-                </div>
-                <form class="user">
-                  <div class="form-group">
-                    <input type="email" class="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address...">
-                  </div>
-                  <div class="form-group">
-                    <input type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password">
-                  </div>
-                  <div class="form-group">
-                    <div class="custom-control custom-checkbox small">
-                      <input type="checkbox" class="custom-control-input" id="customCheck">
-                      <label class="custom-control-label" for="customCheck">Remember Me</label>
+    <div class="row justify-content-center">
+        <div class="col-xl-10 col-lg-12 col-md-9">
+            <div class="card o-hidden border-0 shadow-lg my-5">
+                <div class="card-body p-0">
+                    <!-- Nested Row within Card Body -->
+                    <div class="row">
+                        <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
+                        <div class="col-lg-6">
+                            <div class="p-5">
+                                <div class="text-center">
+                                    <h1 class="h1 text-gray-900 mb-6 font-weight-bold"><?= Yii::$app->name ?></h1>
+                                    <h1 class="h5 text-gray-900 mb-4"><?= Yii::t('app', 'Welcome Back!') ?></h1>
+                                </div>
+                                <?php $form = ActiveForm::begin([]); ?>
+                                <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
+                                <?= $form->field($model, 'password')->passwordInput() ?>
+                                <?= $form->field($model, 'rememberMe')->checkbox() ?>
+                                <div class="form-group">
+                                    <?= Html::submitButton(Yii::t('app', 'Login'), ['class' => 'btn btn-primary btn-block', 'name' => 'login-button']) ?>
+                                </div>
+                                <?php ActiveForm::end(); ?>
+                                <hr>
+                                <div class="text-center">
+                                    <?= Html::a(Yii::t('app', 'Create an Account'), ['signup/index']); ?>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                  </div>
-                  <a href="index.html" class="btn btn-primary btn-user btn-block">
-                      Login
-                    </a>
-                </form>
-                <hr>
-                <div class="text-center">
-                  <a class="small" href="forgot-password.html">Forgot Password?</a>
                 </div>
-                <div class="text-center">
-                  <a class="small" href="register.html">Create an Account!</a>
-                </div>
-              </div>
             </div>
-          </div>
         </div>
-      </div>
-
     </div>
-
-  </div>
-
 </div>
