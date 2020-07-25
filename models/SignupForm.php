ac60bdfc310acd300969f54be7523ffb9b11dcfa
Lucas Emanuel <lucaxemanuel@gmail.com>
2020-07-25 18:22:09 -0300

    :construction: Confirm password when signing up

--- models/SignupForm.php
+++ models/SignupForm.php
@@ -13,10 +13,6 @@ class SignupForm extends Model
     const SCENARIO_COMPANY = "company";
     const SCENARIO_EMPLOYEE = "employee";
 
-    const STEP_ONE = '1';
-    const STEP_TWO = '2';
-
-    public $step = 1;
     public $name;
     public $trade_name;
     public $ein;
@@ -26,27 +22,30 @@ class SignupForm extends Model
     public $ssn;
     public $birthday;
     public $password;
+    public $password_repeat;
 
     public function rules()
     {
         return [
-            [['name', 'ein', 'email', 'step', 'full_name', 'usual_name', 'ssn', 'birthday', 'password'], 'required'],
-            [['password'], 'string', 'max' => 255],
+            [['name', 'ein', 'email', 'full_name', 'usual_name', 'ssn', 'birthday', 'password', 'password_repeat'], 'required'],
+            [['password', 'password_repeat'], 'string', 'max' => 255],
             [['full_name'], 'string', 'max' => 128],
             [['name', 'trade_name', 'email'], 'string', 'max' => 64],
             [['usual_name'], 'string', 'max' => 32],
             [['ein'], 'string', 'max' => 18],
             [['ssn'], 'string', 'max' => 12],
             [['email'], 'email'],
-            [['birthday', 'step'], 'safe'],
+            [['birthday'], 'safe'],
+            [['password'], 'compare'],
+            [['password'], 'compare', 'compareAttribute' => 'password_repeat'],
         ];
     }
 
     public function scenarios()
     {
         $scenarios = parent::scenarios();
-        $scenarios[self::SCENARIO_COMPANY] = ['name', 'ein', 'email', 'trade_name', 'step'];
-        $scenarios[self::SCENARIO_EMPLOYEE] = ['full_name', 'usual_name', 'ssn', 'birthday', 'email', 'password', 'step'];
+        $scenarios[self::SCENARIO_COMPANY] = ['name', 'ein', 'email', 'trade_name'];
+        $scenarios[self::SCENARIO_EMPLOYEE] = ['full_name', 'usual_name', 'ssn', 'birthday', 'email', 'password', 'password_repeat'];
         return $scenarios;
     }
 
@@ -61,7 +60,6 @@ class SignupForm extends Model
             'name' => Yii::t('app', 'Name'),
             'trade_name' => Yii::t('app', 'Trade Name'),
             'ein' => Yii::t('app', 'Ein'),
-            'email' => Yii::t('app', 'Email'),
 
             // Employee
             'full_name' => Yii::t('app', 'Full Name'),
@@ -69,6 +67,9 @@ class SignupForm extends Model
             'ssn' => Yii::t('app', 'Ssn'),
             'birthday' => Yii::t('app', 'Birthday'),
             'password' => Yii::t('app', 'Password'),
+            'password_repeat' => Yii::t('app', 'Password Repeat'),
+
+            'email' => Yii::t('app', 'Email'),
         ];
     }
 
