08864342dfde8f17f46eee1deb746b022d7b628a
Lucas Emanuel <lucaxemanuel@gmail.com>
2020-07-26 13:26:47 -0300

    :construction: Verify if user is guest in index

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -60,6 +60,10 @@ class SiteController extends Controller
      */
     public function actionIndex()
     {
+        if (Yii::$app->user->isGuest) {
+            return $this->redirect(['site/login']);
+        }
+
         return $this->render('index');
     }
 
