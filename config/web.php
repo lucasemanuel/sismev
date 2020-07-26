d83e66f0b645a88a6c026bf2b583999d3a0b8f8c
Lucas Emanuel <lucaxemanuel@gmail.com>
2020-07-26 13:23:47 -0300

    :wrench: Simplifying login url

--- config/web.php
+++ config/web.php
@@ -50,6 +50,7 @@ $config = [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
+                'login' => 'site/login',
             ],
         ],
     ],
