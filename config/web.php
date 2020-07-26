6b310e9b2ce25bcb892ec0d58703fbcd6ad12f8e
Lucas Emanuel <lucaxemanuel@gmail.com>
2020-07-26 13:04:29 -0300

    :wrench: Setting identity class with employee model

--- config/web.php
+++ config/web.php
@@ -23,7 +23,7 @@ $config = [
             'class' => 'yii\caching\FileCache',
         ],
         'user' => [
-            'identityClass' => 'app\models\User',
+            'identityClass' => 'app\models\Employee',
             'enableAutoLogin' => true,
         ],
         'errorHandler' => [
