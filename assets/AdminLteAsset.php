79075bdf8118e8ba034d70b457f9719590966a18
Lucas Emanuel <lucaxemanuel@gmail.com>
2020-08-02 13:44:03 -0300

    :bento: Getting files syle in vendor and adding dependence on FontsAsset)

--- assets/AdminLteAsset.php
+++ assets/AdminLteAsset.php
@@ -3,26 +3,24 @@
 namespace app\assets;
 
 use app\assets\AppAsset;
-use rmrevin\yii\fontawesome\CdnFreeAssetBundle;
+use app\assets\FontsAsset;
 use yii\web\AssetBundle;
 
 class AdminLteAsset extends AssetBundle
 {
-    public $basePath = '@webroot';
-    public $baseUrl = '@web';
+    public $sourcePath = '@vendor/almasaeed2010/adminlte/dist'; 
 
     public $css = [
-        'adminLte/dist/css/adminlte.min.css',
-        'https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback'
+        'css/adminlte.min.css',
     ];
 
     public $js = [
-        'adminLte/dist/js/adminlte.js',
+        'js/adminlte.min.js',
     ];
 
     public $depends = [
         AppAsset::class,
-        CdnFreeAssetBundle::class,
+        FontsAsset::class
     ];
 }
 
