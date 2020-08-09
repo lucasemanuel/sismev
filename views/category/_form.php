c6b9cc9a745e8a525b748440cf6ef390f200c955
Lucas Emanuel <lucaxemanuel@gmail.com>
2020-08-08 21:22:44 -0300

    :construction: Submit category via ajax

--- views/category/_form.php
+++ views/category/_form.php
@@ -1,29 +1,56 @@
 <?php
 
+use app\assets\AxiosAsset;
+use yii\bootstrap4\ActiveForm;
 use yii\helpers\Html;
-use yii\widgets\ActiveForm;
+use yii\widgets\Pjax;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Category */
 /* @var $form yii\widgets\ActiveForm */
+
+AxiosAsset::register($this);
+
+$this->registerJs(
+    <<< 'JS'
+        $('#form_category').on('beforeSubmit', function (e) {
+            const action = $(this).attr('action');
+            const data = $(this).serialize();
+            axios({
+                method: 'post',
+                headers: {'X-Requested-With': 'XMLHttpRequest'},
+                url: action,
+                data: data
+            }).then(function (response) {
+                $.pjax.reload({container: '#update_category'});
+                $('#modal').modal('hide');
+            }).catch(function (e) { 
+                // throw e; 
+            });
+            return false; // prevent default submit
+        });
+    JS,
+    $this::POS_END
+);
+
 ?>
 
 <div class="category-form">
 
-    <?php $form = ActiveForm::begin(); ?>
+    <?php Pjax::begin(['id' => 'update_category']) ?>
+    
+    <?php $form = ActiveForm::begin([
+        'id' => 'form_category',
+    ]); ?>
 
     <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'created_at')->textInput() ?>
-
-    <?= $form->field($model, 'updated_at')->textInput() ?>
-
-    <?= $form->field($model, 'company_id')->textInput() ?>
 
     <div class="form-group">
         <?= Html::submitButton(Yii::t('app', 'Save'), ['class' => 'btn btn-success']) ?>
     </div>
 
     <?php ActiveForm::end(); ?>
+
+    <?php Pjax::end() ?>
 
 </div>
