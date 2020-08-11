6105ab12413d2e3006052bef7e4e78dc9ba48361
Lucas Emanuel <lucaxemanuel@gmail.com>
2020-08-11 15:14:43 -0300

    :construction: Update category create

--- views/category/_form.php
+++ views/category/_form.php
@@ -1,43 +1,15 @@
 <?php
 
-use app\assets\AxiosAsset;
 use yii\bootstrap4\ActiveForm;
 use yii\helpers\Html;
-use yii\widgets\Pjax;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Category */
 /* @var $form yii\widgets\ActiveForm */
 
-AxiosAsset::register($this);
-
-$this->registerJs(
-    <<< 'JS'
-        $('#form_category').on('beforeSubmit', function (e) {
-            const action = $(this).attr('action');
-            const data = $(this).serialize();
-            axios({
-                method: 'post',
-                headers: {'X-Requested-With': 'XMLHttpRequest'},
-                url: action,
-                data: data
-            }).then(function (response) {
-                $.pjax.reload({container: '#update_category'});
-                $('#modal').modal('hide');
-            }).catch(function (e) { 
-                // throw e; 
-            });
-            return false; // prevent default submit
-        });
-    JS,
-    $this::POS_END
-);
-
 ?>
 
 <div class="category-form">
-
-    <?php Pjax::begin(['id' => 'update_category']) ?>
     
     <?php $form = ActiveForm::begin([
         'id' => 'form_category',
@@ -50,7 +22,5 @@ $this->registerJs(
     </div>
 
     <?php ActiveForm::end(); ?>
-
-    <?php Pjax::end() ?>
 
 </div>
