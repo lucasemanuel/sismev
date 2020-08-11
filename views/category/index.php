43b4c38db907965c2993034bbec10f4335a1fe9c
Lucas Emanuel <lucaxemanuel@gmail.com>
2020-08-11 10:35:08 -0300

    :construction: Gridview categories responsive

--- views/category/index.php
+++ views/category/index.php
@@ -1,9 +1,8 @@
 <?php
 
 use yii\helpers\Html;
-use yii\grid\GridView;
+use kartik\grid\GridView;
 use yii\helpers\Url;
-use yii\widgets\Pjax;
 
 /* @var $this yii\web\View */
 /* @var $searchModel app\models\CategorySearch */
@@ -26,33 +25,37 @@ $this->registerJs(
 )
 ?>
 <div class="category-index">
+    <div class="row">
+        <div class="col">
+            <h1><?= Html::encode($this->title) ?></h1>
 
-    <h1><?= Html::encode($this->title) ?></h1>
+            <p>
+                <?= Html::button(Yii::t('app', 'Create Category'), ['value' => Url::to(['create']), 'class' => 'btn btn-success', 'id' => 'btn-modal']) ?>
+            </p>
 
-    <p>
-        <?= Html::button(Yii::t('app', 'Create Category'), ['value' => Url::to(['create']), 'class' => 'btn btn-success', 'id' => 'btn-modal']) ?>
-    </p>
+            <?= $this->render('@app/views/layouts/modal.php', ['options' => ['title' => Yii::t('app', 'Category')]]) ?>
 
-    <?= $this->render('@app/views/layouts/modal.php', ['options' => ['title' => Yii::t('app', 'Category')]]) ?>
+            <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+            <?= GridView::widget([
+                'dataProvider' => $dataProvider,
+                'filterModel' => $searchModel,
+                'pjax' => true,
+                'responsive' => true,
+                'responsiveWrap' => false,
+                'columns' => [
+                    ['class' => 'kartik\grid\SerialColumn'],
 
-    <?php Pjax::begin(['id' => 'pjax_categories']); ?>
-    <?= GridView::widget([
-        'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
-        'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
+                    'name',
+                    'created_at',
+                    'updated_at',
 
-            'id',
-            'name',
-            'created_at',
-            'updated_at',
-            'company_id',
-
-            ['class' => 'yii\grid\ActionColumn'],
-        ],
-    ]); ?>
-    <?php Pjax::end(); ?>
-
+                    [
+                        'class' => 'kartik\grid\ActionColumn',
+                        'template' => '{update} {delete}',
+                    ],
+                ],
+            ]); ?>
+        </div>
+    </div>
 </div>
