c3ae9face9ff7acb72f28e599466387ac25e846b
Lucas Emanuel <lucaxemanuel@gmail.com>
2020-08-18 10:14:38 -0300

    :construction: Open modal to select category

--- views/product/index.php
+++ views/product/index.php
@@ -2,6 +2,7 @@
 
 use yii\helpers\Html;
 use yii\grid\GridView;
+use yii\helpers\Url;
 
 /* @var $this yii\web\View */
 /* @var $searchModel app\models\ProductSearch */
@@ -9,14 +10,19 @@ use yii\grid\GridView;
 
 $this->title = Yii::t('app', 'Products');
 $this->params['breadcrumbs'][] = $this->title;
+
+$this->registerJsFile('@web/js/modal.js', ['depends' => [yii\web\JqueryAsset::class]]);
+
 ?>
 <div class="product-index">
 
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        <?= Html::a(Yii::t('app', 'Create Product'), ['create'], ['class' => 'btn btn-success']) ?>
+        <?= Html::a(Yii::t('app', 'Create Product'), '#', ['value' => Url::to(['category']), 'class' => 'btn btn-success btn-modal', 'data-toggle' => 'modal']) ?>
     </p>
+
+    <?= $this->render('@app/views/layouts/modal.php', ['options' => ['title' => Yii::t('app', 'Category')]]) ?>
 
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
@@ -29,7 +35,7 @@ $this->params['breadcrumbs'][] = $this->title;
             'id',
             'code',
             'name',
-            'unit_price',
+            'unit_price:currency',
             'amount',
             //'max_amount',
             //'min_amount',
