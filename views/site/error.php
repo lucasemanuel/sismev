3f02dfedf8ea79dfce8f71c4e75fd97888d4e76d
Lucas Emanuel <lucaxemanuel@gmail.com>
2020-08-24 10:55:48 -0300

    :lipstick: Update error view

--- views/site/error.php
+++ views/site/error.php
@@ -6,22 +6,44 @@
 /* @var $exception Exception */
 
 use yii\helpers\Html;
+use yii\helpers\Url;
 
 $this->title = $name;
+
+$this->registerCss(
+    <<< CSS
+    .error-page > .error-content {
+        margin: 0;
+    }
+
+    .error-messages {
+        margin-left: 20px;
+    }
+
+    .headline {
+        font-size: 100px;
+        float: left;
+    }    
+    CSS
+);
 ?>
 <div class="site-error">
-
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <div class="alert alert-danger">
-        <?= nl2br(Html::encode($message)) ?>
-    </div>
-
-    <p>
-        The above error occurred while the Web server was processing your request.
-    </p>
-    <p>
-        Please contact us if you think this is a server error. Thank you.
-    </p>
-
+    <section class="content">
+        <div class="error-page">
+            <h4 class="font-weight-light text-center">
+                <i class="fas fa-exclamation-triangle text-warning"></i> <?= nl2br(Html::encode($message)) ?>
+            </h4>
+            <div class="error-content d-flex align-items-center">
+                <h2 class="headline text-warning">#<?= nl2br(Html::encode($exception->statusCode)) ?></h2>
+                <span class="error-messages">
+                    <?= Yii::t('app', 'The above error occurred while the web server was processing your request.') ?>
+                    <p>
+                        <?= Yii::t('app', 'Return to the <a href="{url}">main page here</a>.', [
+                            'url' => Url::to(['index'])
+                        ]); ?>
+                    </p>
+                </span>
+            </div>
+        </div>
+    </section>
 </div>
