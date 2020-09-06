922d2a3392a912c487350cab9d9e3903731cead5
Lucas Emanuel <lucaxemanuel@gmail.com>
2020-09-06 10:35:37 -0300

    :lipstick: Update styles

--- views/operation/_search.php
+++ views/operation/_search.php
@@ -1,39 +1,52 @@
 <?php
 
+use kartik\form\ActiveForm;
 use yii\helpers\Html;
-use yii\widgets\ActiveForm;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\OperationSearch */
 /* @var $form yii\widgets\ActiveForm */
 ?>
+<?php $form = ActiveForm::begin([
+    'action' => ['index'],
+    'method' => 'get',
+]); ?>
+<!-- <div class="card card-outline card-secondary collapsed-card operation-search"> -->
+<div class="card card-outline card-secondary operation-search">
+    <div class="card-header">
+        <h3 class="card-title"><?= Yii::t('app', 'Advanced Search') ?></h3>
+        <div class="card-tools">
+            <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                <i class="fas fa-minus"></i>
+            </button>
+        </div>
+    </div> <!-- /.card-body -->
+    <div class="card-body">
+        <?= $form->field($model, 'amount') ?>
 
-<div class="operation-search">
+        <?= $form->field($model, 'setting_amount')->radioList([
+            0 => Yii::t('app', 'Specific amount'),
+            1 => Yii::t('app', 'From amount'),
+            2 => Yii::t('app', 'Up to')
+        ]) ?>
 
-    <?php $form = ActiveForm::begin([
-        'action' => ['index'],
-        'method' => 'get',
-    ]); ?>
+        <?= $form->field($model, 'created_at') ?>
 
-    <?= $form->field($model, 'id') ?>
+        <?php // echo $form->field($model, 'updated_at') 
+        ?>
 
-    <?= $form->field($model, 'in_out') ?>
-
-    <?= $form->field($model, 'amount') ?>
-
-    <?= $form->field($model, 'reason') ?>
-
-    <?= $form->field($model, 'created_at') ?>
-
-    <?php // echo $form->field($model, 'updated_at') ?>
-
-    <?php // echo $form->field($model, 'product_id') ?>
-
-    <div class="form-group">
-        <?= Html::submitButton(Yii::t('app', 'Search'), ['class' => 'btn btn-primary']) ?>
-        <?= Html::resetButton(Yii::t('app', 'Reset'), ['class' => 'btn btn-outline-secondary']) ?>
+        <?php // echo $form->field($model, 'product_id') 
+        ?>
+    </div><!-- /.card-body -->
+    <div class="card-footer">
+        <div class="row">
+            <div class="col">
+                <?= Html::submitButton(Yii::t('app', 'Search'), ['class' => 'btn btn-primary']) ?>
+            </div>
+            <div class="col d-flex justify-content-end">
+                <?= Html::a('Reset', ['index'], ['class' => 'btn btn-outline-secondary align-self-end']) ?>
+            </div>
+        </div>
     </div>
-
-    <?php ActiveForm::end(); ?>
-
 </div>
+<?php ActiveForm::end(); ?>
--- views/operation/index.php
+++ views/operation/index.php
@@ -1,7 +1,8 @@
 <?php
 
+use kartik\grid\GridView;
 use yii\helpers\Html;
-use yii\grid\GridView;
+use yii\helpers\Url;
 
 /* @var $this yii\web\View */
 /* @var $searchModel app\models\OperationSearch */
@@ -9,34 +10,69 @@ use yii\grid\GridView;
 
 $this->title = Yii::t('app', 'Operations');
 $this->params['breadcrumbs'][] = $this->title;
+
+$gridColumns = [
+    ['class' => 'kartik\grid\SerialColumn'],
+    'in_out:InputOrOutput',
+    'reason',
+    'created_at',
+    //'product_id',
+    [
+        'attribute' => 'product_id',
+        'value' => function ($model) {
+            return $model->product;
+        }
+    ],
+    [
+        'attribute' => 'amount',
+        'format' => 'amount',
+        'filterType' => GridView::FILTER_NUMBER, 
+        'filterWidgetOptions' => [
+            'maskedInputOptions' => [
+                'allowMinus' => false,
+                'rightAlign' => false,
+            ]
+        ],
+    ],
+    [
+        'class' => 'kartik\grid\ActionColumn',
+        'width' => '100px',
+    ],
+];
+
 ?>
 <div class="operation-index">
+    <div class="row">
+        <div class="col">            
+            <?php echo $this->render('_search', ['model' => $searchModel]); ?>
 
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <p>
-        <?= Html::a(Yii::t('app', 'Create Operation'), ['create'], ['class' => 'btn btn-success']) ?>
-    </p>
-
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
-
-    <?= GridView::widget([
-        'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
-        'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
-
-            'id',
-            'in_out',
-            'amount',
-            'reason',
-            'created_at',
-            //'updated_at',
-            //'product_id',
-
-            ['class' => 'yii\grid\ActionColumn'],
-        ],
-    ]); ?>
-
-
+            <?= GridView::widget([
+                'dataProvider' => $dataProvider,
+                'filterModel' => $searchModel,
+                'columns' => $gridColumns,
+                'responsive' => true,
+                'responsiveWrap' => false,
+                'hover' => true,
+                'toolbar' =>  [
+                    [
+                        'content' =>
+                            Html::button('<i class="fas fa-plus"></i>', [
+                                'value' => Url::to(['category']),
+                                'class' => 'btn btn-success btn-modal',
+                                'title' => Yii::t('app', 'Add Product'),
+                            ]),
+                        'options' => ['class' => 'btn-group mr-2']
+                    ],
+                    '{toggleData}',
+                ],
+                'panel' => [
+                    'type' => GridView::TYPE_DEFAULT,
+                    'heading' => Html::encode($this->title),
+                    // 'headingOptions' => ['class' => ''],
+                    // 'footer' => false,
+                    'afterOptions' => ['class' => ''],
+                ],
+            ]); ?>
+        </div>
+    </div>
 </div>
