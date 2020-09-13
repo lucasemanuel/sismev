63a4936bcc1def3812617522d217b0fa372d599a
Lucas Emanuel <lucaxemanuel@gmail.com>
2020-09-13 01:58:16 -0300

    :lipstick: Update styles employee index

--- views/employee/index.php
+++ views/employee/index.php
@@ -1,7 +1,8 @@
 <?php
 
-use yii\helpers\Html;
 use kartik\grid\GridView;
+use yii\helpers\Html;
+use yii\helpers\Url;
 use yii\widgets\MaskedInput;
 
 /* @var $this yii\web\View */
@@ -10,70 +11,75 @@ use yii\widgets\MaskedInput;
 
 $this->title = Yii::t('app', 'Employees');
 $this->params['breadcrumbs'][] = $this->title;
+
+$gridColumns = [
+    ['class' => 'kartik\grid\SerialColumn'],
+    'full_name',
+    [
+        'attribute' => 'ssn',
+        'filterType' => MaskedInput::class,
+        'filterWidgetOptions' => [
+            'mask' => ['999.999.999-99'],
+        ],
+    ],
+    [
+        'attribute' => 'birthday',
+        'format' => 'date',
+        'filterType' => GridView::FILTER_DATE,
+        'filterWidgetOptions' => [
+            'pluginOptions' => [
+                'format' => 'dd/mm/yyyy',
+            ]
+        ],
+    ],
+    'email',
+    [
+        'attribute' => 'phone_number',
+        'filterType' => MaskedInput::class,
+        'filterWidgetOptions' => [
+            'mask' => ['(99) 9999-9999', '(99) 99999-9999'],
+        ]
+    ],
+    [
+        'attribute' => 'is_manager',
+        'class' => '\kartik\grid\BooleanColumn',
+        'trueLabel' => Yii::t('app', 'Yes'),
+        'falseLabel' => Yii::t('app', 'No')
+    ],
+    [
+        'class' => 'kartik\grid\ActionColumn',
+        'width' => '100px',
+    ],
+];
 ?>
-<div class="employee-index">
-
-    <!-- <h1><?= Html::encode($this->title) ?></h1> -->
-
+<div class="employee-index row">
+    <div class="col">
+        <?= GridView::widget([
+            'dataProvider' => $dataProvider,
+            'filterModel' => $searchModel,
+            'columns' => $gridColumns,
+            'responsive' => true,
+            'responsiveWrap' => false,
+            'hover' => true,
+            'toolbar' =>  [
+                [
+                    'content' =>
+                    Html::a('<i class="fas fa-plus"></i>', Url::to(['create']), [
+                        'class' => 'btn btn-success',
+                        'title' => Yii::t('app', 'Add Employee'),
+                    ]),
+                    'options' => ['class' => 'btn-group mr-2']
+                ],
+                '{toggleData}',
+            ],
+            'panel' => [
+                'type' => GridView::TYPE_DEFAULT,
+                'heading' => Html::encode($this->title),
+                // 'headingOptions' => ['class' => ''],
+                // 'footer' => false,
+                'afterOptions' => ['class' => ''],
+            ],
+        ]); ?>
+    </div>
     <p>
-        <!-- <?= Html::a(Yii::t('app', 'Create Employee'), ['create'], ['class' => 'btn btn-success']) ?> -->
-    </p>
-
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
-
-    <?php
-    echo GridView::widget([
-        'dataProvider'=> $dataProvider,
-        'filterModel' => $searchModel,
-        'columns' => [
-            ['class' => 'kartik\grid\SerialColumn'],
-            'full_name',
-            [
-                'attribute' => 'ssn',
-                'filterType' => MaskedInput::class,
-                'filterWidgetOptions' => [
-                    'mask' => ['999.999.999-99'],
-
-                ],
-            ],
-            [
-                'attribute' => 'birthday',
-                'format' => 'date',
-                'filterType' => GridView::FILTER_DATE, 
-                'filterWidgetOptions' => [
-                    'pluginOptions' => [
-                        'format' => 'dd/mm/yyyy',
-                    ]
-                ],
-            ],
-            'email',
-            [
-                'attribute' => 'phone_number',
-                'filterType' => MaskedInput::class,
-                'filterWidgetOptions' => [
-                    'mask' => ['(99) 9999-9999', '(99) 99999-9999'],
-                ]
-            ],
-            [
-                'class' => 'kartik\grid\ActionColumn',
-            ],
-        ],
-        'hover' => true,
-        // 'toolbar' => [
-        //     '{export}',
-        //     '{toggleData}'
-        // ],
-        // 'toggleDataContainer' => ['class' => 'btn-group-sm'],
-        // 'exportContainer' => ['class' => 'btn-group-sm'],
-        'panel' => [
-            'heading' => '<h3 class="panel-title">'.Html::encode($this->title).'</h3>',
-            'type' => 'default',
-            'before'=> Html::a(Yii::t('app', 'Create Employee'), ['create'], ['class' => 'btn btn-success']),
-            // 'after'=>Html::a('<i class="fas fa-redo"></i> Reset Grid', ['index'], ['class' => 'btn btn-info']),
-            'footer'=>false
-        ],
-    ]);
-    ?>
-
-
 </div>
