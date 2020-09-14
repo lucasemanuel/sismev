77a59ccdd872e84a4cd13bc76050ca2330445a57
Lucas Emanuel <lucaxemanuel@gmail.com>
2020-09-14 13:56:04 -0300

    :card_file_box: Add column 'is_paid' in expense

--- models/Expense.php
+++ models/Expense.php
@@ -16,6 +16,7 @@ use yii\db\Expression;
  * @property string|null $description
  * @property float $value
  * @property string $payday
+ * @property int|null $is_paid
  * @property string|null $paid_at
  * @property string $created_at
  * @property string|null $updated_at
@@ -64,6 +65,8 @@ class Expense extends ActiveRecord
             [['payday', 'paid_at', 'created_at', 'updated_at'], 'safe'],
             [['payday'], 'default', 'value' => null],
             [['company_id'], 'integer'],
+            [['is_paid'], 'boolean'],
+            [['is_paid'], 'default', 'value' => 0],
             [['name'], 'string', 'max' => 64],
             [['company_id'], 'exist', 'skipOnError' => true, 'targetClass' => Company::class, 'targetAttribute' => ['company_id' => 'id']],
         ];
@@ -80,6 +83,7 @@ class Expense extends ActiveRecord
             'description' => Yii::t('app', 'Description'),
             'value' => Yii::t('app', 'Value'),
             'payday' => Yii::t('app', 'Payday'),
+            'is_paid' => Yii::t('app', 'Is Paid'),
             'paid_at' => Yii::t('app', 'Paid At'),
             'created_at' => Yii::t('app', 'Created At'),
             'updated_at' => Yii::t('app', 'Updated At'),
