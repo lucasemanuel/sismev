d53aaba85d9a9e22d4c969f325de76a01f78fc10
Lucas Emanuel <lucaxemanuel@gmail.com>
2020-09-14 14:02:32 -0300

    :lipstick: Update expense views

--- views/expense/index.php
+++ views/expense/index.php
@@ -1,7 +1,8 @@
 <?php
 
+use kartik\grid\GridView;
 use yii\helpers\Html;
-use yii\grid\GridView;
+use yii\helpers\Url;
 
 /* @var $this yii\web\View */
 /* @var $searchModel app\models\ExpenseSearch */
@@ -9,36 +10,88 @@ use yii\grid\GridView;
 
 $this->title = Yii::t('app', 'Expenses');
 $this->params['breadcrumbs'][] = $this->title;
+
+$gridColumns = [
+    ['class' => 'kartik\grid\SerialColumn'],
+    'name',
+    'description:ntext',
+    [
+        'attribute' => 'value',
+        'format' => 'currency',
+        'filterType' => GridView::FILTER_NUMBER, 
+        'filterWidgetOptions' => [
+            'maskedInputOptions' => [
+                'prefix' => Yii::$app->formatter->getCurrencySymbol()." ",
+                'allowMinus' => false,
+                'rightAlign' => false,
+            ]
+        ],
+    ],
+    [
+        'attribute' => 'payday',
+        'format' => 'date',
+        'filterType' => GridView::FILTER_DATE,
+        'filterWidgetOptions' => [
+            'pluginOptions' => [
+                'format' => 'dd/mm/yyyy',
+            ]
+        ],
+    ],
+    [
+        'attribute' => 'paid_at',
+        'format' => 'date',
+        'filterType' => GridView::FILTER_DATE,
+        'filterWidgetOptions' => [
+            'pluginOptions' => [
+                'format' => 'dd/mm/yyyy',
+            ]
+        ],
+    ],
+    [
+        'attribute' => 'is_paid',
+        'class' => '\kartik\grid\BooleanColumn',
+        'trueLabel' => Yii::t('app', 'Yes'),
+        'falseLabel' => Yii::t('app', 'No'),
+        'width' => '100px'
+    ],
+    [
+        'class' => 'kartik\grid\ActionColumn',
+        'width' => '100px',
+    ],
+];
+
 ?>
 <div class="expense-index">
+    <div class="row">
+        <div class="col">            
+            <?php //echo $this->render('_search', ['model' => $searchModel]); ?>
 
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <p>
-        <?= Html::a(Yii::t('app', 'Create Expense'), ['create'], ['class' => 'btn btn-success']) ?>
-    </p>
-
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
-
-    <?= GridView::widget([
-        'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
-        'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
-
-            'id',
-            'name',
-            'description:ntext',
-            'value',
-            'payday',
-            //'paid_at',
-            //'created_at',
-            //'updated_at',
-            //'company_id',
-
-            ['class' => 'yii\grid\ActionColumn'],
-        ],
-    ]); ?>
-
-
+            <?= GridView::widget([
+                'dataProvider' => $dataProvider,
+                'filterModel' => $searchModel,
+                'columns' => $gridColumns,
+                'responsive' => true,
+                'responsiveWrap' => false,
+                'hover' => true,
+                'toolbar' =>  [
+                    [
+                        'content' =>
+                            Html::a('<i class="fas fa-plus"></i>', Url::to(['create']),[
+                                'class' => 'btn btn-success',
+                                'title' => Yii::t('app', 'Add Operation'),
+                            ]),
+                        'options' => ['class' => 'btn-group mr-2']
+                    ],
+                    '{toggleData}',
+                ],
+                'panel' => [
+                    'type' => GridView::TYPE_DEFAULT,
+                    'heading' => Html::encode($this->title),
+                    // 'headingOptions' => ['class' => ''],
+                    // 'footer' => false,
+                    'afterOptions' => ['class' => ''],
+                ],
+            ]); ?>
+        </div>
+    </div>
 </div>
--- views/expense/view.php
+++ views/expense/view.php
@@ -1,6 +1,7 @@
 <?php
 
 use yii\helpers\Html;
+use yii\helpers\Url;
 use yii\widgets\DetailView;
 
 /* @var $this yii\web\View */
@@ -9,36 +10,89 @@ use yii\widgets\DetailView;
 $this->title = $model->name;
 $this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Expenses'), 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
-\yii\web\YiiAsset::register($this);
+
+$this->registerCssFile('@web/css/detailView.css');
+$this->registerJsFile('@web/js/modal.js', ['depends' => [yii\web\JqueryAsset::class]]);
+
 ?>
 <div class="expense-view">
+    <div class="row">
+        <div class="col-12">
+            <?= $this->render('@app/views/layouts/modal.php', ['options' => ['title' => Yii::t('app', 'Category')]]) ?>
 
-    <h1><?= Html::encode($this->title) ?></h1>
+            <div class="card card-outline">
+                <div class="card-header border-0">
+                    <h3 class="card-title"><?= Yii::t('app', 'General Data') ?></h3>
+                    <div class="card-tools">
+                        <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                            <i class="fas fa-minus"></i>
+                        </button>
+                    </div>
+                </div> <!-- /.card-body -->
+                <div class="card-body table-responsive p-0">
+                    <?= DetailView::widget([
+                        'model' => $model,
+                        'attributes' => [
+                            'name',
+                            'description',
+                            'is_paid:boolean',
+                            [
+                                'visible' => $model->is_paid,
+                                'attribute' => 'paid_at',
+                                'format' => 'date',
+                            ],
+                            'value:currency',
+                            'payday:date'
+                        ],
+                    ]) ?>
+                </div><!-- /.card-body -->
+            </div>
+        </div>
+    </div>
 
-    <p>
-        <?= Html::a(Yii::t('app', 'Update'), ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a(Yii::t('app', 'Delete'), ['delete', 'id' => $model->id], [
-            'class' => 'btn btn-danger',
-            'data' => [
-                'confirm' => Yii::t('app', 'Are you sure you want to delete this item?'),
-                'method' => 'post',
-            ],
-        ]) ?>
-    </p>
+    <div class="row">
+        <div class="col-12">
+            <div class="card card-outline">
+                <div class="card-header border-0">
+                    <h3 class="card-title"><?= Yii::t('app', 'System Data') ?></h3>
+                    <div class="card-tools">
+                        <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                            <i class="fas fa-minus"></i>
+                        </button>
+                    </div>
+                </div> <!-- /.card-body -->
+                <div class="card-body table-responsive p-0">
+                    <?= DetailView::widget([
+                        'model' => $model,
+                        'attributes' => [
+                            'created_at:datetime',
+                            'updated_at:datetime',
+                        ],
+                    ]) ?>
+                </div><!-- /.card-body -->
+            </div>
+        </div>
+    </div>
 
-    <?= DetailView::widget([
-        'model' => $model,
-        'attributes' => [
-            'id',
-            'name',
-            'description:ntext',
-            'value',
-            'payday',
-            'paid_at',
-            'created_at',
-            'updated_at',
-            'company_id',
-        ],
-    ]) ?>
-
+    <div class="row">
+        <div class="col-12 d-flex justify-content-end">
+            <p>
+                <?= Html::a(Yii::t('app', 'Update'), ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
+                <?= Html::a(Yii::t('app', 'Delete'), ['delete', 'id' => $model->id], [
+                    'class' => 'btn btn-danger',
+                    'data' => [
+                        'confirm' => Yii::t('app', 'Are you sure you want to delete this item?'),
+                        'method' => 'post',
+                    ],
+                ]) ?>
+                <?php if (!$model->is_paid): ?>
+                <?= Html::a(Yii::t('app', 'Pay'), Url::to(['pay', 'id' => $model->id]), [
+                    'class' => 'btn btn-success btn-modal', 
+                    'value' => Url::to(['pay', 'id' => $model->id]),
+                    'data-toggle' => 'modal'
+                ]); ?>
+                <?php endif; ?>
+            </p>
+        </div>
+    </div>
 </div>
