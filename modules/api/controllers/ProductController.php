edada4a59d98f1453e48ae787f3d3b5f4297e1df
Lucas Emanuel <lucaxemanuel@gmail.com>
2020-10-04 18:05:37 -0300

    :sparkles: Add api module

---
+++ modules/api/controllers/ProductController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace app\modules\api\controllers;
+
+use app\models\ProductSearch;
+use yii\filters\ContentNegotiator;
+use yii\web\Controller;
+use yii\web\Response;
+
+class ProductController extends Controller
+{
+    public function behaviors()
+    {
+        return [
+            [
+                'class' => ContentNegotiator::class,
+                'formats' => [
+                    'application/json' => Response::FORMAT_JSON,
+                ],
+            ],
+        ];
+    }
+
+    public function actionQuery($q = null, $id = null)
+    {
+        $out = ['results' => ['id' => '', 'name' => '', 'price' => '']];
+
+        if (!is_null($q)) {
+            $data = ProductSearch::findInNameWithVariation(trim($q));
+            $out['results'] = array_map(function($product) {
+                return $product->toArray();
+            }, array_values($data));
+        }
+        return $out;
+    }
+}
