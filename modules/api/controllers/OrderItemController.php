9d01c39324b91a127cd39af2b7502d7cfbca244c
Lucas Emanuel <lucaxemanuel@gmail.com>
2020-10-05 16:38:35 -0300

    :sparkles: Add validation action order item

--- modules/api/controllers/OrderItemController.php
+++ modules/api/controllers/OrderItemController.php
@@ -3,14 +3,13 @@
 namespace app\modules\api\controllers;
 
 use app\models\OrderItem;
-use app\models\OrderItemSearch;
 use Yii;
 use yii\filters\ContentNegotiator;
 use yii\filters\VerbFilter;
 use yii\web\BadRequestHttpException;
 use yii\web\Controller;
-use yii\web\NotFoundHttpException;
 use yii\web\Response;
+use yii\widgets\ActiveForm;
 
 class OrderItemController extends Controller
 {
@@ -27,45 +26,12 @@ class OrderItemController extends Controller
                 'class' => VerbFilter::class,
                 'actions' => [
                     'create' => ['POST'],
+                    'validation' => ['POST'],
                 ],
             ],
         ];
     }
 
-    /**
-     * Lists all OrderItem models.
-     * @return mixed
-     */
-    public function actionIndex()
-    {
-        $searchModel = new OrderItemSearch();
-        $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
-
-        return $this->render('index', [
-            'searchModel' => $searchModel,
-            'dataProvider' => $dataProvider,
-        ]);
-    }
-
-    /**
-     * Displays a single OrderItem model.
-     * @param integer $order_id
-     * @param integer $product_id
-     * @return mixed
-     * @throws NotFoundHttpException if the model cannot be found
-     */
-    public function actionView($order_id, $product_id)
-    {
-        return $this->render('view', [
-            'model' => $this->findModel($order_id, $product_id),
-        ]);
-    }
-
-    /**
-     * Creates a new OrderItem model.
-     * If creation is successful, the browser will be redirected to the 'view' page.
-     * @return mixed
-     */
     public function actionCreate()
     {
         $model = new OrderItem();
@@ -81,56 +47,13 @@ class OrderItemController extends Controller
         throw new BadRequestHttpException($erro);
     }
 
-    /**
-     * Updates an existing OrderItem model.
-     * If update is successful, the browser will be redirected to the 'view' page.
-     * @param integer $order_id
-     * @param integer $product_id
-     * @return mixed
-     * @throws NotFoundHttpException if the model cannot be found
-     */
-    public function actionUpdate($order_id, $product_id)
+    public function actionValidation()
     {
-        $model = $this->findModel($order_id, $product_id);
+        $model = new OrderItem();
 
-        if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->redirect(['view', 'order_id' => $model->order_id, 'product_id' => $model->product_id]);
+        if (Yii::$app->request->isAjax && $model->load(Yii::$app->request->post())) {
+            Yii::$app->response->format = Response::FORMAT_JSON;
+            return ActiveForm::validate($model);
         }
-
-        return $this->render('update', [
-            'model' => $model,
-        ]);
-    }
-
-    /**
-     * Deletes an existing OrderItem model.
-     * If deletion is successful, the browser will be redirected to the 'index' page.
-     * @param integer $order_id
-     * @param integer $product_id
-     * @return mixed
-     * @throws NotFoundHttpException if the model cannot be found
-     */
-    public function actionDelete($order_id, $product_id)
-    {
-        $this->findModel($order_id, $product_id)->delete();
-
-        return $this->redirect(['index']);
-    }
-
-    /**
-     * Finds the OrderItem model based on its primary key value.
-     * If the model is not found, a 404 HTTP exception will be thrown.
-     * @param integer $order_id
-     * @param integer $product_id
-     * @return OrderItem the loaded model
-     * @throws NotFoundHttpException if the model cannot be found
-     */
-    protected function findModel($order_id, $product_id)
-    {
-        if (($model = OrderItem::findOne(['order_id' => $order_id, 'product_id' => $product_id])) !== null) {
-            return $model;
-        }
-
-        throw new NotFoundHttpException(Yii::t('app', 'The requested page does not exist.'));
     }
 }
