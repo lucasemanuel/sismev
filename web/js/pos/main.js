664a840bb4a2994cb59c97b632745e7921138ebe
Lucas Emanuel <lucaxemanuel@gmail.com>
2020-10-05 16:53:12 -0300

    :construction: Add push item in front-end

--- web/js/pos/main.js
+++ web/js/pos/main.js
@@ -1,5 +1,36 @@
+const app = new Vue({
+    el: '.pos-index',
+    data: {
+        listOrderItems: [],
+        total: 0,
+    },
+    methods: {
+        pushItem() {
+            const form = document.querySelector('form');
+            const formData = new FormData(form);
+            axios.post('/api/order-item/create', formData)
+                .then(({ data }) => {
+                    console.log(data);
+                })
+                .catch(({ response }) => {
+                    showToast(response.data.name, response.data.message)
+                });
+        }
+    }
+});
+
+function showToast(title, message = '') {
+    $(document).Toasts('create', {
+        title: title,
+        body: message,
+        autohide: true,
+        delay: 4000,
+        class: ['bg-warning', 'fix-toast']
+    });
+}
+
 function setPrice(value) {
     $('#product-unit_price-disp').inputmask("setvalue", value);
-    $('#orderproduct-unit_price-disp').inputmask("setvalue", value);
-    $('#orderproduct-unit_price-disp').focus();
+    $('#orderItem-unit_price-disp').inputmask("setvalue", value);
+    $('#orderItem-unit_price-disp').focus();
 }
