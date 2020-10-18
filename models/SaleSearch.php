f431935250900ff0892e604efdba933ac442a86b
Lucas Emanuel <lucaxemanuel@gmail.com>
2020-10-18 13:13:58 -0300

    :sparkles: Add filters for sale search

--- models/SaleSearch.php
+++ models/SaleSearch.php
@@ -5,6 +5,8 @@ namespace app\models;
 use yii\base\Model;
 use yii\data\ActiveDataProvider;
 use app\models\Sale;
+use Yii;
+use yii\db\ActiveQuery;
 
 /**
  * SaleSearch represents the model behind the search form of `app\models\Sale`.
@@ -12,6 +14,8 @@ use app\models\Sale;
 class SaleSearch extends Sale
 {
     public $order_total_value;
+    public $setting_search_order_total_value;
+    public $status = [ 'sold', 'canceled' ];
 
     /**
      * {@inheritdoc}
@@ -19,9 +23,9 @@ class SaleSearch extends Sale
     public function rules()
     {
         return [
-            [['id', 'is_sold', 'is_canceled', 'employee_id', 'order_id'], 'integer'],
+            [['id', 'is_sold', 'is_canceled', 'order_id', 'setting_search_order_total_value'], 'integer'],
             [['amount_paid', 'discount', 'order_total_value'], 'number'],
-            [['sale_at', 'canceled_at', 'updated_at'], 'safe'],
+            [['sale_at', 'canceled_at', 'updated_at', 'employee_id', 'status'], 'safe'],
         ];
     }
 
@@ -47,6 +51,8 @@ class SaleSearch extends Sale
 
         // add conditions that should always apply here
 
+        $query->innerJoin('employee', 'sale.employee_id = employee.id');
+
         $this->is_sold = 1;
 
         $dataProvider = new ActiveDataProvider([
@@ -61,6 +67,10 @@ class SaleSearch extends Sale
             'order_total_value' => [
                 'asc' => ['order.total_value' => SORT_ASC],
                 'desc' => ['order.total_value' => SORT_DESC],
+            ],
+            'employee_id' => [
+                'asc' => ['employee.usual_name' => SORT_ASC],
+                'desc' => ['employee.usual_name' => SORT_DESC],
             ],
         ]);
 
@@ -78,16 +88,55 @@ class SaleSearch extends Sale
             'amount_paid' => $this->amount_paid,
             'discount' => $this->discount,
             'is_sold' => $this->is_sold,
-            'is_canceled' => $this->is_canceled,
-            'sale_at' => $this->sale_at,
             'canceled_at' => $this->canceled_at,
             'updated_at' => $this->updated_at,
-            'employee_id' => $this->employee_id,
-            'order.total_value' => $this->order_total_value 
         ]);
 
         $query->andFilterWhere(['like', 'order.code', $this->order_id]);
+        $query->andFilterWhere(['like', 'employee.usual_name', $this->employee_id]);
+
+        $this->filterDate($query);
+        $this->filterOrderValue($query);
+        $this->filterStatusSale($query);
 
         return $dataProvider;
+    }
+
+    private function filterDate(ActiveQuery &$query)
+    {
+        if (!empty($this->sale_at)) {
+            $dates = explode(" - ", $this->sale_at);
+            $start = Yii::$app->formatter->asDateTimeDefault($dates[0]);
+            $end = Yii::$app->formatter->asDateTimeDefault($dates[1]);
+
+            $query->andFilterWhere(['between', 'sale.sale_at', $start, $end]);
+        }
+    }
+
+    private function filterOrderValue(ActiveQuery &$query)
+    {
+        $operator = '=';
+        if (isset($this->order_total_value)) {
+            if ($this->setting_search_order_total_value == 1)
+                $operator = '>=';
+            else if ($this->setting_search_order_total_value == 2)
+                $operator = '<=';
+        }
+
+        $query->andFilterWhere([$operator, 'order.total_value', $this->order_total_value]);
+    }
+
+    private function filterStatusSale(ActiveQuery &$query)
+    {
+        if (!empty($this->status) && count($this->status) < 2) {
+            $value = current($this->status);
+            
+            if ($value == 'canceled')
+                $query->andFilterWhere(['is_canceled' => 1]);
+            else
+                $query->andFilterWhere(['is_canceled' => 0]);
+        } else {
+            $this->status = [ 'sold', 'canceled' ];
+        }
     }
 }
