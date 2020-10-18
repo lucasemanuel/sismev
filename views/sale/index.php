5ed72bd95ccd5759cfd3bfb9c4ae924b7314b475
Lucas Emanuel <lucaxemanuel@gmail.com>
2020-10-18 14:22:07 -0300

    :lipstick: Update action column style in sale list

--- views/sale/index.php
+++ views/sale/index.php
@@ -2,6 +2,7 @@
 
 use kartik\grid\GridView;
 use yii\helpers\Html;
+use yii\helpers\Url;
 
 /* @var $this yii\web\View */
 /* @var $searchModel app\models\SaleSearch */
@@ -45,12 +46,35 @@ $gridColumns = [
         'format' => 'html',
         'value' => function ($model) {
             if (!$model->is_canceled)
-                return '<span class="badge bg-success text-uppercase">'.Yii::t('app', 'Sold').'</span>';
-            return '<span class="badge bg-danger text-uppercase">'.Yii::t('app', 'Canceled').'</span>';
+                return '<span class="badge bg-success text-uppercase">' . Yii::t('app', 'Sold') . '</span>';
+            return '<span class="badge bg-danger text-uppercase">' . Yii::t('app', 'Canceled') . '</span>';
         }
     ],
     [
         'class' => 'kartik\grid\ActionColumn',
+        'template' => '{view} {delete}',
+        'deleteOptions' => [
+            'label' => '<span class="fas fa-times-circle"></span>',
+            'data-confirm' => Yii::t('app', 'Are you sure you want to cancel this sale?') . '<br>' . Yii::t('app', 'All transactions made because of this sale will be reversed.'),
+            'title' => Yii::t('app', 'Invoice'),
+            'aria-label' => Yii::t('app', 'Invoice'),
+        ],
+        'viewOptions' => [
+            'label' => '<span class="fas fa-file-invoice"></span>',
+            'title' => Yii::t('app', 'Invoice'),
+            'aria-label' => Yii::t('app', 'Invoice'),
+        ],
+        'urlCreator' => function ($action, $model, $key, $index) {
+            if ($action === 'view')
+                return Url::to(['invoice', 'id' => $model->id]);
+            if ($action === 'delete')
+                return Url::to(['canceled', 'id' => $model->id]);
+        },
+        'visibleButtons' => [
+            'delete' => function ($model) { 
+                return !$model->is_canceled;
+            }
+        ],
         'width' => '100px',
     ],
 ];
