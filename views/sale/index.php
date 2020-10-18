88176ca7105baf5753de167371a69471a24b435d
Lucas Emanuel <lucaxemanuel@gmail.com>
2020-10-18 10:27:23 -0300

    :lipstick: Update sale list

--- views/sale/index.php
+++ views/sale/index.php
@@ -1,7 +1,7 @@
 <?php
 
+use kartik\grid\GridView;
 use yii\helpers\Html;
-use yii\grid\GridView;
 
 /* @var $this yii\web\View */
 /* @var $searchModel app\models\SaleSearch */
@@ -9,34 +9,75 @@ use yii\grid\GridView;
 
 $this->title = Yii::t('app', 'Sales');
 $this->params['breadcrumbs'][] = $this->title;
+
+$gridColumns = [
+    ['class' => 'kartik\grid\SerialColumn'],
+    [
+        'attribute' => 'order_id',
+        'value' => function ($model) {
+            return $model->order->code;
+        },
+    ],
+    [
+        'attribute' => 'order_total_value',
+        'format' => 'currency',
+        'value' => function ($model) {
+            return $model->order->total_value;
+        },
+        'pageSummary' => true,
+    ],
+    [
+        'attribute' => 'amount_paid',
+        'format' => 'currency',
+        'pageSummary' => true,
+    ],
+    [
+        'label' => Yii::t('app', 'Cashier'),
+        'attribute' => 'employee_id',
+        'value' => function ($model) {
+            return $model->employee->usual_name;
+        },
+    ],
+    'sale_at:datetime',
+    [
+        'label' => Yii::t('app', 'Status'),
+        'attribute' => 'is_sold',
+        'format' => 'html',
+        'value' => function ($model) {
+            if (!$model->is_canceled)
+                return '<span class="badge bg-success text-uppercase">'.Yii::t('app', 'Sold').'</span>';
+            return '<span class="badge bg-danger text-uppercase">'.Yii::t('app', 'Canceled').'</span>';
+        }
+    ],
+    [
+        'class' => 'kartik\grid\ActionColumn',
+        'width' => '100px',
+    ],
+];
+
 ?>
 <div class="sale-index">
+    <div class="row">
+        <div class="col">
+            <?php //echo $this->render('_search', ['model' => $searchModel]); 
+            ?>
 
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <p>
-        <?= Html::a(Yii::t('app', 'Create Sale'), ['create'], ['class' => 'btn btn-success']) ?>
-    </p>
-
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
-
-    <?= GridView::widget([
-        'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
-        'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
-
-            'id',
-            'amount_paid',
-            'discount',
-            'sale_at',
-            'canceled_at',
-            //'updated_at',
-            //'order_id',
-
-            ['class' => 'yii\grid\ActionColumn'],
-        ],
-    ]); ?>
-
-
+            <?= GridView::widget([
+                'dataProvider' => $dataProvider,
+                'columns' => $gridColumns,
+                'responsive' => true,
+                'responsiveWrap' => false,
+                'hover' => true,
+                'showPageSummary' => true,
+                'toolbar' =>  [
+                    '{toggleData}',
+                ],
+                'panel' => [
+                    'type' => GridView::TYPE_DEFAULT,
+                    'heading' => Html::encode($this->title),
+                    'afterOptions' => ['class' => ''],
+                ],
+            ]); ?>
+        </div>
+    </div>
 </div>
