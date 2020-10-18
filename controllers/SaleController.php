bbb3e4f0ac071aa9e403c01e6070fd5826d372bc
Lucas Emanuel <lucaxemanuel@gmail.com>
2020-10-18 14:21:02 -0300

    :truck: Rename action invoice

--- controllers/SaleController.php
+++ controllers/SaleController.php
@@ -23,7 +23,7 @@ class SaleController extends Controller
             'verbs' => [
                 'class' => VerbFilter::class,
                 'actions' => [
-                    'delete' => ['POST'],
+                    'canceled' => ['POST'],
                 ],
             ],
         ];
@@ -44,68 +44,15 @@ class SaleController extends Controller
         ]);
     }
 
-    /**
-     * Displays a single Sale model.
-     * @param integer $id
-     * @return mixed
-     * @throws NotFoundHttpException if the model cannot be found
-     */
-    public function actionView($id)
+    public function actionInvoice($id)
     {
-        return $this->render('view', [
+        return $this->render('invoice', [
             'model' => $this->findModel($id),
         ]);
     }
 
-    /**
-     * Creates a new Sale model.
-     * If creation is successful, the browser will be redirected to the 'view' page.
-     * @return mixed
-     */
-    public function actionCreate()
+    public function actionCanceled($id)
     {
-        $model = new Sale();
-
-        if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->redirect(['view', 'id' => $model->id]);
-        }
-
-        return $this->render('create', [
-            'model' => $model,
-        ]);
-    }
-
-    /**
-     * Updates an existing Sale model.
-     * If update is successful, the browser will be redirected to the 'view' page.
-     * @param integer $id
-     * @return mixed
-     * @throws NotFoundHttpException if the model cannot be found
-     */
-    public function actionUpdate($id)
-    {
-        $model = $this->findModel($id);
-
-        if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->redirect(['view', 'id' => $model->id]);
-        }
-
-        return $this->render('update', [
-            'model' => $model,
-        ]);
-    }
-
-    /**
-     * Deletes an existing Sale model.
-     * If deletion is successful, the browser will be redirected to the 'index' page.
-     * @param integer $id
-     * @return mixed
-     * @throws NotFoundHttpException if the model cannot be found
-     */
-    public function actionDelete($id)
-    {
-        $this->findModel($id)->delete();
-
         return $this->redirect(['index']);
     }
 
