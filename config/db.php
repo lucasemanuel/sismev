116ff1764349f37d98b793d16d2ce3671a1cf33f
Lucas Emanuel <lucaxemanuel@gmail.com>
2020-11-16 18:26:17 -0300

    :ambulance: Fix connection with database

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=sismec',
+    'dsn' => 'mysql:host=127.0.0.1;dbname=sismec',
     'username' => 'sismec',
     'password' => 'sismec',
     'charset' => 'utf8',
