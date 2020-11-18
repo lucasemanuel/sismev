f12726415005e072c5bf0a35bd6d9f403dcea521
Lucas Emanuel <lucaxemanuel@gmail.com>
2020-11-18 09:50:13 -0300

    :lipstick: Add chart with sales info

--- views/site/index.php
+++ views/site/index.php
@@ -2,27 +2,36 @@
 
 /* @var $this yii\web\View */
 
-$this->title = 'My Yii Application';
-\app\assets\AxiosAsset::register($this);
+use app\assets\AxiosAsset;
+use app\assets\ChartAsset;
+
+$this->title = 'Dashboard';
+
+AxiosAsset::register($this);
+ChartAsset::register($this);
 
 $this->registerJs(
     <<<JS
     axios.get('/api/sale/week').then(({ data }) => {
-        console.log(data);
+        const { amount_paid, dates, total_sale } = data;
         var ctx = document.getElementById('salesChart').getContext('2d');
         var chart = new Chart(ctx, {
             type: 'line',
             data: {
-                labels: data.dates,
+                labels: dates,
                 datasets: [{
                     lineTension: 0.2,
-                    label: 'Gráfico',
-                    // backgroundColor: ['green', 'blue', 'yellow'],
-                    borderColor: 'rgb(255, 99, 132)',
-                    data: data.values,
+                    label: amount_paid['label'],
+                    borderColor: '#28a745',
+                    data: amount_paid['values'],
+                },
+                {
+                    lineTension: 0.2,
+                    label: total_sale['label'],
+                    borderColor: '#dc3545',
+                    data: total_sale['values'],
                 }]
             },
-    
             options: {
                 scales: {
                     yAxes: [{
@@ -38,202 +47,71 @@ $this->registerJs(
 JS,
     $this::POS_END
 );
+
 ?>
 <div class="site-index">
     <div class="row">
-        <div class="col-12 col-sm-6 col-md-3">
+        <div class="col-12 col-sm-6">
             <div class="info-box">
-                <span class="info-box-icon bg-info elevation-1"><i class="fas fa-cog"></i></span>
-
-                <div class="info-box-content">
-                    <span class="info-box-text">CPU Traffic</span>
-                    <span class="info-box-number">
-                  10
-                  <small>%</small>
-                </span>
-                </div>
-                <!-- /.info-box-content -->
-            </div>
-            <!-- /.info-box -->
-        </div>
-        <!-- /.col -->
-        <div class="col-12 col-sm-6 col-md-3">
-            <div class="info-box mb-3">
-                <span class="info-box-icon bg-danger elevation-1"><i class="fas fa-thumbs-up"></i></span>
-
-                <div class="info-box-content">
-                    <span class="info-box-text">Likes</span>
-                    <span class="info-box-number">41,410</span>
-                </div>
-                <!-- /.info-box-content -->
-            </div>
-            <!-- /.info-box -->
-        </div>
-        <!-- /.col -->
-
-        <!-- fix for small devices only -->
-        <div class="clearfix hidden-md-up"></div>
-
-        <div class="col-12 col-sm-6 col-md-3">
-            <div class="info-box mb-3">
                 <span class="info-box-icon bg-success elevation-1"><i class="fas fa-shopping-cart"></i></span>
-
                 <div class="info-box-content">
-                    <span class="info-box-text">Sales</span>
-                    <span class="info-box-number">760</span>
+                    <span class="info-box-text"><?= Yii::t('app', 'Total sales') ?></span>
+                    <span class="info-box-number">
+                        <?= $datas['sales'] ?>
+                        <small>(<?= Yii::t('app', 'Today')?>)</small>
+                    </span>
                 </div>
-                <!-- /.info-box-content -->
             </div>
-            <!-- /.info-box -->
         </div>
-        <!-- /.col -->
-        <div class="col-12 col-sm-6 col-md-3">
+
+        <div class="col-12 col-sm-6">
             <div class="info-box mb-3">
-                <span class="info-box-icon bg-warning elevation-1"><i class="fas fa-users"></i></span>
+                <span class="info-box-icon bg-danger elevation-1"><i class="fas fa-hand-holding-usd"></i></span>
 
                 <div class="info-box-content">
-                    <span class="info-box-text">New Members</span>
-                    <span class="info-box-number">2,000</span>
+                    <span class="info-box-text"><?= Yii::t('app', 'Sales value') ?></span>
+                    <span class="info-box-number">
+                        <?= $datas['total_sales_price'] ?>
+                        <small>(<?= Yii::t('app', 'Today')?>)</small>
+                    </span>
                 </div>
-                <!-- /.info-box-content -->
             </div>
-            <!-- /.info-box -->
         </div>
-        <!-- /.col -->
     </div>
 
     <div class="row">
-        <div class="col-md-12">
+        <div class="d-none d-sm-block col">
             <div class="card">
                 <div class="card-header">
-                    <h5 class="card-title">Monthly Recap Report</h5>
-
+                    <h5 class="card-title">
+                        <?= Yii::t('app', 'Last week') ?>
+                        (<span class="font-weight-bold"><?= Yii::t('app', 'Sales') ?></span>)
+                    </h5>
                     <div class="card-tools">
                         <button type="button" class="btn btn-tool" data-card-widget="collapse">
                             <i class="fas fa-minus"></i>
                         </button>
-                        <div class="btn-group">
-                            <button type="button" class="btn btn-tool dropdown-toggle" data-toggle="dropdown">
-                                <i class="fas fa-wrench"></i>
-                            </button>
-                            <div class="dropdown-menu dropdown-menu-right" role="menu">
-                                <a href="#" class="dropdown-item">Action</a>
-                                <a href="#" class="dropdown-item">Another action</a>
-                                <a href="#" class="dropdown-item">Something else here</a>
-                                <a class="dropdown-divider"></a>
-                                <a href="#" class="dropdown-item">Separated link</a>
-                            </div>
-                        </div>
                         <button type="button" class="btn btn-tool" data-card-widget="remove">
                             <i class="fas fa-times"></i>
                         </button>
                     </div>
                 </div>
-                <!-- /.card-header -->
                 <div class="card-body">
                     <div class="row">
-                        <div class="col-md-8">
+                        <div class="col">
                             <p class="text-center">
-                                <strong>Sales: 1 Jan, 2014 - 30 Jul, 2014</strong>
+                                <strong>
+                                    <?= Yii::$app->formatter->asDate(strtotime("-1 week")) ?>
+                                    - <?= Yii::$app->formatter->asDate(strtotime("-1 day")) ?>
+                                </strong>
                             </p>
-
                             <div class="chart">
-                                <!-- Sales Chart Canvas -->
                                 <canvas id="salesChart" height="180" style="height: 180px; display: block; width: 638px;" width="638" class="chartjs-render-monitor"></canvas>
                             </div>
-                            <!-- /.chart-responsive -->
-                        </div>
-                        <!-- /.col -->
-                        <div class="col-md-4">
-                            <p class="text-center">
-                                <strong>Goal Completion</strong>
-                            </p>
-
-                            <div class="progress-group">
-                                Add Products to Cart
-                                <span class="float-right"><b>160</b>/200</span>
-                                <div class="progress progress-sm">
-                                    <div class="progress-bar bg-primary" style="width: 80%"></div>
-                                </div>
-                            </div>
-                            <!-- /.progress-group -->
-
-                            <div class="progress-group">
-                                Complete Purchase
-                                <span class="float-right"><b>310</b>/400</span>
-                                <div class="progress progress-sm">
-                                    <div class="progress-bar bg-danger" style="width: 75%"></div>
-                                </div>
-                            </div>
-
-                            <!-- /.progress-group -->
-                            <div class="progress-group">
-                                <span class="progress-text">Visit Premium Page</span>
-                                <span class="float-right"><b>480</b>/800</span>
-                                <div class="progress progress-sm">
-                                    <div class="progress-bar bg-success" style="width: 60%"></div>
-                                </div>
-                            </div>
-
-                            <!-- /.progress-group -->
-                            <div class="progress-group">
-                                Send Inquiries
-                                <span class="float-right"><b>250</b>/500</span>
-                                <div class="progress progress-sm">
-                                    <div class="progress-bar bg-warning" style="width: 50%"></div>
-                                </div>
-                            </div>
-                            <!-- /.progress-group -->
-                        </div>
-                        <!-- /.col -->
-                    </div>
-                    <!-- /.row -->
-                </div>
-                <!-- ./card-body -->
-                <div class="card-footer">
-                    <div class="row">
-                        <div class="col-sm-3 col-6">
-                            <div class="description-block border-right">
-                                <span class="description-percentage text-success"><i class="fas fa-caret-up"></i> 17%</span>
-                                <h5 class="description-header">$35,210.43</h5>
-                                <span class="description-text">TOTAL REVENUE</span>
-                            </div>
-                            <!-- /.description-block -->
-                        </div>
-                        <!-- /.col -->
-                        <div class="col-sm-3 col-6">
-                            <div class="description-block border-right">
-                                <span class="description-percentage text-warning"><i class="fas fa-caret-left"></i> 0%</span>
-                                <h5 class="description-header">$10,390.90</h5>
-                                <span class="description-text">TOTAL COST</span>
-                            </div>
-                            <!-- /.description-block -->
-                        </div>
-                        <!-- /.col -->
-                        <div class="col-sm-3 col-6">
-                            <div class="description-block border-right">
-                                <span class="description-percentage text-success"><i class="fas fa-caret-up"></i> 20%</span>
-                                <h5 class="description-header">$24,813.53</h5>
-                                <span class="description-text">TOTAL PROFIT</span>
-                            </div>
-                            <!-- /.description-block -->
-                        </div>
-                        <!-- /.col -->
-                        <div class="col-sm-3 col-6">
-                            <div class="description-block">
-                                <span class="description-percentage text-danger"><i class="fas fa-caret-down"></i> 18%</span>
-                                <h5 class="description-header">1200</h5>
-                                <span class="description-text">GOAL COMPLETIONS</span>
-                            </div>
-                            <!-- /.description-block -->
                         </div>
                     </div>
-                    <!-- /.row -->
                 </div>
-                <!-- /.card-footer -->
             </div>
-            <!-- /.card -->
         </div>
-        <!-- /.col -->
     </div>
 </div>
